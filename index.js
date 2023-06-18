8dd776453cd0ca5967b9125a7481af3de5038c55
willianwt2000 <willianmetelien@gmail.com>
2023-06-17 19:27:10 -1200

    agregando logica de encryptar

---
+++ index.js
@@ -0,0 +1,44 @@
+const inputMensaje = document.querySelector("#encryptar")
+const resultado = document.querySelector("#decrypt")
+
+const encryptar  = document.getElementById("encrypt-btn")
+const desencryptar  = document.getElementById("decrypt-btn")
+const copiar  = document.getElementById("copy")
+
+
+// Funciones para encryptar y desencryprat su
+
+function encryptarTexto() {
+    var mensaje = inputMensaje.value;
+    var mensajeEncryptado = mensaje
+    .replaceAll("e", "enter")
+    .replaceAll("i","imes")
+    .replaceAll("o","ober")
+    .replaceAll("a","ai")
+    .replaceAll("u","ufat")
+
+    resultado.value = mensajeEncryptado;
+}
+
+function desencryptarTexto() {
+    var mensajeEncryptado = inputMensaje.value;
+    var mensaje = mensajeEncryptado    
+    .replaceAll("enter", "e")
+    .replaceAll("imes","i")
+    .replaceAll("ober","o")
+    .replaceAll("ai","a")
+    .replaceAll("ufat","u")
+
+    resultado.value = mensaje;
+}
+
+
+// Agregar evento de click al botón de copiar
+copiar.addEventListener("click", function() {
+    // Seleccionar el resultado
+    resultado.select();
+    resultado.setSelectionRange(0, 99999); // Para dispositivos móviles
+
+    // Copiar el resultado al portapapeles
+    document.execCommand("copy");
+});
