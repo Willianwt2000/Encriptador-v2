97b8b2361cbb28d03117ecefcb11ae59b0332294
willianwt2000 <willianmetelien@gmail.com>
2023-06-18 06:51:11 -1200

    agregando funcion luz en progreso...

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const encryptar  = document.getElementById("encrypt-btn")
 const desencryptar  = document.getElementById("decrypt-btn")
 const copiar  = document.getElementById("copy")
 
+const luz = document.getElementById("contenedorTextos");
+
+
 
 // Funciones para encryptar y desencryprat su
 
@@ -42,3 +45,23 @@ copiar.addEventListener("click", function() {
     // Copiar el resultado al portapapeles
     document.execCommand("copy");
 });
+
+// Funcion para apagar y prender luz
+
+let isNegro = false; // Variable de estado inicializada como false
+
+luz.onclick = function() {
+    if (isNegro) {
+        // Si el texto es negro, cambiar a otro color
+        luz.style.background = "white";
+        luz.style.color = "white";
+        luz.querySelector("h1").style.color = "black";
+        isNegro = false; // Actualizar la variable de estado
+    } else {
+        // Si el texto no es negro, ponerlo en negro
+        luz.style.background = "black";
+        luz.style.color = "black";
+        luz.querySelector("h1").style.color = "white";
+        isNegro = true; // Actualizar la variable de estado
+    }
+    };
